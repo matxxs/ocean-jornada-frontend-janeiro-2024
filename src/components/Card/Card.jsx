a87be633a2cd1ee0875ef96018e506e34ed1cba2
Matheus Santos <124845403+matxxs@users.noreply.github.com>
2024-01-25 18:40:04 +0000

    Atualizando informações do Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,14 @@ function Card(){
     return (
         <>
           <div className = "card">
-            <h2>Rick Sanchez</h2>
+            <h2> Rick Sanchez </h2>
+
+            <div className="bio">
+                <h3> Status: Vivo </h3>
+                <h3>Espécie: Humana </h3>
+                <h3> Origem: Terra C-137 </h3>   
+            </div>
+
             <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick Sanchez" />
           </div>
         </>

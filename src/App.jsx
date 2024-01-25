e1c643d3c55e9374cd24b0aa2fb4dcda3cf8215b
Matheus Santos <124845403+matxxs@users.noreply.github.com>
2024-01-25 15:11:04 +0000

    Criação do arquivo Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card/Card'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className = "card">
-        <h2>Rick Sanchez</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick Sanchez" />
-      </div>
+      <Card />
     </>
   )
 }

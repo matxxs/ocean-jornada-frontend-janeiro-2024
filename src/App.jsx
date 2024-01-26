26ac0dcce0350402c169d851e88398832158c82f
Matheus Santos <124845403+matxxs@users.noreply.github.com>
2024-01-26 01:18:37 +0000

    Add Item ao Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
 import './App.css'
-import Card from './components/Card/Card'
-
+import Card from './components/Card/Card'; 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Card />
+      <Card item={item1} />
+      <Card item={item2} />
     </>
   )
 }
